538d5652266c1d487102adecffce3a9b645db6aa
Tomoya Hoshino <tomooya1010@outlook.com>
2018-07-31 07:05:07 +0000

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
 
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/contact', to: 'static_pages#contact'
+  
+  resources :users
 end
