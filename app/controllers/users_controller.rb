63c446a461da3d21c9ecd2200aa9818b968b5ce9
Tomoya Hoshino <tomooya1010@outlook.com>
2018-08-01 06:30:43 +0000

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,10 +10,11 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Sample App"
-       redirect_to user_url(@user)
+      redirect_to user_url(@user)
     else
-       render "new"
+      render "new"
     end
   end
   
@@ -21,7 +22,7 @@ class UsersController < ApplicationController
   
     def user_params
       params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+                                :password_confirmation)
     end
   
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
 
@@ -7,6 +9,9 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/contact', to: 'static_pages#contact'
-  
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :users
 end
